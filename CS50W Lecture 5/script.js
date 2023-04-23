bb1f27f27b34d275e764bf780e586a673c36218c
blueberrydev <jhonatanwcom@gmail.com>
2023-04-23 18:39:04 -0300

    New things added and learned!!

--- CS50W Lecture 5/script.js
+++ CS50W Lecture 5/script.js
@@ -14,6 +14,7 @@ let elementos = {
   12: "Terceito tempo do Fogo",
 };
 
+// MONTHS BUTTON
 function count() {
   if (counter == 12) {
     counter = 0;
@@ -22,12 +23,15 @@ function count() {
   const countText = document.querySelector("#monthText");
   countText.innerHTML = `Mês: ${counter}, Elemento: ${elementos[counter]} `;
 }
-
 document.addEventListener("DOMContentLoaded", () => {
   const buttonCount = document.querySelector("#monthButton");
   buttonCount.onclick = count;
 });
 
+// SWITCH
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#solluaButton").onclick = switchSolLua;
+});
 function switchSolLua() {
   const heading = document.querySelector("#solluaText");
   if (heading.innerHTML === "Sol!") {
@@ -37,9 +41,36 @@ function switchSolLua() {
   }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("form").onsubmit = function () {
+//HELLO BUTTON
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#formHi").onsubmit = () => {
     const name = document.querySelector("#nameButton").value;
-    alert(`Hello, ${name}!`);
+    alert(`Hello, ${name}`);
+
+    // Stop form from submitting
+    return false;
+  };
+});
+
+// COLORS SELECTOR
+document.addEventListener("DOMContentLoaded", () => {
+  document.querySelector("#colorSelector").onchange = function () {
+    document.querySelector("#colorsText").style.color = this.value;
+  };
+});
+
+//TASKS LIST
+document.addEventListener("DOMContentLoaded", function () {
+  document.querySelector("#formTasks").onsubmit = () => {
+    const task = document.querySelector("#task").value;
+    console.log(task);
+
+    let li = document.createElement("li");
+    li.innerHTML = task;
+
+    document.querySelector("#formTasks").append(li);
+
+    // Stop form from submitting
+    return false;
   };
 });
