9efa11711d0c618209e9dff9c0560f9a5f37cf13
blueberrydev <jhonatanwcom@gmail.com>
2023-05-07 22:23:33 -0300

    Gratidão

--- CS50W Lecture 7/prime.py
+++ CS50W Lecture 7/prime.py
@@ -2,8 +2,8 @@ import math
 
 def is_prime(n):
   if n<2:
-   return False
-  for i in range(2, int(math.sqrt(n))):
+    return False
+  for i in range(2, int(math.sqrt(n))+1):
    if n % i == 0:
     return False
-  return True
+  return False
