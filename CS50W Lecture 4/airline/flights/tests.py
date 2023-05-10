12fdb22f2ab6fad19c38c62d33f28abee2a696dd
blueberrydev <jhonatanwcom@gmail.com>
2023-05-09 21:51:03 -0300

    Testing Airports

--- CS50W Lecture 4/airline/flights/tests.py
+++ CS50W Lecture 4/airline/flights/tests.py
@@ -1,3 +1,17 @@
+from django.db.models import Max
 from django.test import TestCase
 
+from .models import Airport, Flight, Passenger
+
 # Create your tests here.
+class FlightTestCase(TestCase):
+
+ def setup(self):
+  #Create airports.
+  a1 = Airport.objects.create(code="AAA", city="City A")
+  a2 = Airport.objects.create(code="BBB", city="City B")
+
+  # Create fligths.
+  Flight.objects.create(origin=a1, destination=a2, duration=100)
+  Flight.objects.create(origin=a1, destination=a1, duration=200)
+  Flight.objects.create(origin=a1, destination=a2, duration=-100)
