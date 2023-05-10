fb6700b2e1d7bebc50f3ddf111cfd093dc3eb4b0
blueberrydev <jhonatanwcom@gmail.com>
2023-05-09 22:24:02 -0300

    Testing Aiports code. Needs changes

--- CS50W Lecture 4/airline/flights/tests.py
+++ CS50W Lecture 4/airline/flights/tests.py
@@ -1,5 +1,5 @@
 from django.db.models import Max
-from django.test import TestCase
+from django.test import Client, TestCase
 
 from .models import Airport, Flight, Passenger
 
@@ -15,3 +15,28 @@ class FlightTestCase(TestCase):
   Flight.objects.create(origin=a1, destination=a2, duration=100)
   Flight.objects.create(origin=a1, destination=a1, duration=200)
   Flight.objects.create(origin=a1, destination=a2, duration=-100)
+ 
+ def test_departures_count(self):
+  a= Airport.objects.get(code="AAA")
+  self.assertEqual(a.departures.count(), 3)
+
+ def test_arrivals_count(self):
+  a = Airport.objects.get(code="AAA")
+  self.assertEqual(a.arrivals.count(), 1)
+
+ def test_valid_flight(self):
+  a1 = Airport.objects.get(code="AAA")
+  a2 = Airport.objects.get(code="BBB")
+  f = light.objects.get(origin=a1, destination=a2, duration=100)
+  self.assertTrue(f.is_valid_flight())
+
+ def test_invalid_flight_destination(self):
+  a1= Airport.objects.get(code="AAA")
+  f= Fligth.objects.get(origin=a1, destination=a1)
+  self.assertFalse(f.is_valid_flight())
+
+ def test_invalid_flight_duration(self):
+  a1= Airport.objects.get(code="AAA")
+  a2 = Airport.objects.get(code="BBB")
+  f= Fligth.objects.get(origin=a1, destination=a2, duration=-100)
+  self.assertFalse(f.is_valid_flight())
