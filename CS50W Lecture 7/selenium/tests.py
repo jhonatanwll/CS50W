1d93ac8e977a762e1c2c625a28c16271cc44c2d0
blueberrydev <jhonatanwcom@gmail.com>
2023-05-14 12:23:48 -0300

    testing

--- CS50W Lecture 7/selenium/tests.py
+++ CS50W Lecture 7/selenium/tests.py
@@ -17,22 +17,22 @@ class WebpageTests(unittest.TestCase):
     
     def test_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element("id", "increase")
         increase.click()
-        self.assertEqaul(driver.find_element_by_tag_name("h1").text, "1")
+        self.assertEqual(driver.find_element("tag", "h1").text, "1")
 
     def test_decrease(self):
         driver.get(file_uri("counter.html"))
-        decrease = driver.find_element_by_id("decrease")
+        decrease = driver.find_element("id", "decrease")
         decrease.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").ext, "-1")
+        self.assertEqual(driver.find_element("tag", "h1").text, "-1")
 
     def test_multiple_increase(self):
         driver.get(file_uri("counter.html"))
-        increase = driver.find_element_by_id("increase")
+        increase = driver.find_element("id", "increase")
         for i in range(3):
             increase.click()
-        self.assertEqual(driver.find_element_by_tag_name("h1").text, "3")
+        self.assertEqual(driver.find_element("tag", "h1").text, "3")
 
 if __name__ == "__main__":
     unittest.main()
